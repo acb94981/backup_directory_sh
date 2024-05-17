773188731a68c8cbfe5d1cbc3b56eab836a6ee10
acb94981 <acb94981@gmail.com>
2024-05-18 01:51:24 +0330

    first commit_backup.sh project

---
+++ README.txt
@@ -0,0 +1,4 @@
+run in windows :
+1.download git bash and more bash apps in windows
+2.move backup.sh to your directory
+3.write bash backup.sh or ./backup.sh in your git bash 
---
+++ backup.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+read -p "your sure about that backup your file ? y/n " answer
+
+if [ "$answer" == "y" ]
+then    
+    read -p "Do create your file to zip or rar ? zip/rar " answer2
+    if [ "$answer2" == "zip" ]
+    then    
+        mkdir backup
+        cp * backup
+        tar -cvf backup.zip backup
+        cd backup
+        rm *
+        cd ..
+        rmdir backup
+        mkdir backup
+        mv backup.zip backup
+    elif [ "$answer2" == "rar" ]
+        mkdir backup
+        cp * backup
+        tar -cvf backup.rar backup
+        cd backup
+        rm *
+        cd ..
+        rmdir backup
+        mkdir backup
+        mv backup.zip backup
+    fi     
+fi
