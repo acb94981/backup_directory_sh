cfaec50af04f3b6f6a1461c7023631f8c3a85333
acb94981 <acb94981@gmail.com>
2024-05-21 21:47:41 +0330

    last ....

--- backup.sh
+++ backup.sh
@@ -1,18 +1,16 @@
 #! /bin/bash
 
-
-
-
 echo "
 1. backup
 2. format directory
 3. help(README)
-
+4. 
 
 "
 read once_answer
 
 if [ $once_answer = "3" ];then
+    read -p "your sure about that backup your file ? y/n " help_input
     cat README.txt
 elif [ $once_answer = "1" ];then
     read -p "your sure about that backup your file ? y/n " answer
@@ -41,6 +39,15 @@ elif [ $once_answer = "1" ];then
         fi 
     else 
     echo "ok! you are not make backup!"    
+    fi
+elif [ $once_answer = "2" ];then
+    read -p "are sure about that ? y/n " once_answer_2
+    if [ $once_answer_2 = y ];then
+        echo "Ok!"
+        rm *
+        rmdir *
+    else
+        echo "Ok!"
     fi
 fi
 
